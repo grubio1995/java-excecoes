40ad418e4da5ed3d7fe94250daf9ce8375e4dfdc
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-03-22 22:26:10 -0300

    Try Catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,22 +1,28 @@
 public class Fluxo {
 
-    public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		metodo1();
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		try {
+			metodo2();
+		} catch (ArithmeticException ex) {
+			System.out.println("ArithmeticException");
+		}
 
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Fim do metodo2");
-    }
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+		}
+		System.out.println("Fim do metodo2");
+	}
 }
