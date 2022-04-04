48a56df072992597bdd0860c2259784a3052ed17
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-04-03 21:59:03 -0300

    Sacando com Checked Exception

--- bytebank-herdado-conta/src/TesteSaca.java
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -5,7 +5,11 @@ public class TesteSaca {
 		Conta conta = new ContaCorrente(123, 321);
 		
 		conta.deposita(200.0);
-		conta.saca(210);
+		try {
+			conta.saca(210);
+		} catch (SaldoInsuficienteException e) {
+			System.out.println("Ex: " + e.getMessage());
+		}
 		
 		System.out.println(conta.getSaldo());
 	}
